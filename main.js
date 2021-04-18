0d9fa18ee0e78e78d384deadc7db3c9b8415a27d
Azkazikna <70270081+Azkazikna@users.noreply.github.com>
2021-04-19 00:55:38 +0700

    first commit

--- main.js
+++ main.js
@@ -47,3 +47,16 @@ tooltips.forEach(tooltip => {
         })
     })
 })
+
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close');
+window.onload = function(){
+    setTimeout(function() {
+        popup.style.display = "block"
+        //add some time delay to show popup
+    }, 2000)
+}
+
+close.addEventListener('click', () => {
+    popup.style.display = "none";
+})
